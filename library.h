d73bc1aaebfad0f8b87edbd633b151a9fdf1031c
Bishesh Khanal <bishesh.khanal25@gmail.com>
2022-10-30 21:14:16 +0545

    library.h has been created
    The file containing the prototypes of all the functions used in the project has been created

---
+++ library.h
@@ -0,0 +1,6 @@
+char setLevel();
+void setSudoku( int[][9], int[][9], char );
+void takeSudoku( int[][9], int[][9] );
+void checkSudoku( int[][9], int[][9], bool );
+void transpose( int[][9] );
+void display( int[][9] );
